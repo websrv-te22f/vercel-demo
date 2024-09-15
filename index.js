4d7c0af263724d94dc6fa0e764ef9f1fd7c1c11f
Johan Kellén <johan.kellen@utb.linkoping.se>
2024-09-15 19:53:31 +0000

    ip

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get('/', (req, res)=>{
     console.log('GET till /');
     res.send('GET till /');
 });
+
+app.get('/ip',(req,res)=>{
+    console.log(req.ip);
+    res.send(`Hej ${req.ip}`);
+});
+
+app.all('*', (req, res)=>{
+    res.send('matchade ingen rutt');
+})
 
 app.listen(port, ()=>{
     console.log(`Servern igång på ${port}`);
